64840efca50733b7ed4baf9a9db60fdb103aad0a
rszelc <37050723+rszelc@users.noreply.github.com>
2019-06-06 17:27:40 +0200

    Add files via upload

---
+++ Gra w życie/gameoflive2/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace gameoflive2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rozmiar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "50";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wzór:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "niezmienny",
+            "glider",
+            "oscylator",
+            "losowy"});
+            this.comboBox1.Location = new System.Drawing.Point(288, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Wstaw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dodaj:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(551, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(596, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(641, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Ok";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ruchy:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(74, 46);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(55, 22);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "100";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(19, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 276);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(205, 10);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(23, 22);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "10";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(237, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(334, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(144, 10);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(55, 22);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "50";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Gra w życie/gameoflive2/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gameoflive2
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        Timer T = new Timer();
+        int licz = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            T.Interval = 100;
+            T.Tick += new EventHandler(T_Tick);
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            int moves = Int32.Parse(textBox4.Text);
+            
+            move(moves, licz);
+            licz++;
+        }
+
+        SolidBrush brush = new SolidBrush(Color.);
+        Bitmap bmp;
+        Graphics gr;
+        int[,] tab;
+        int[,] tab2;
+        int sizex;
+        int sizey;
+        int px;
+        
+        void next()
+        {
+            for (int i = 0; i < sizex; i++)
+            {
+                for (int j = 0; j < sizey; j++)
+                {
+                    check(i, j);
+                }
+            }
+            for (int i = 0; i < sizex; i++)
+            {
+                for (int j = 0; j < sizey; j++)
+                {
+                    tab[i,j] = tab2[i,j];
+                    tab2[i,j] = 0;
+                }
+            }
+
+        }
+        void check(int x, int y)
+        {
+            int temp = 0;
+            int cell = isAlive(x, y);
+            if (x == 0 && y == 0)
+            {
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x + 1,y + 1] == 1) temp++;
+                if (tab[x,sizey - 1] == 1) temp++;
+                if (tab[x + 1,sizey - 1] == 1) temp++;
+                if (tab[sizex-1, sizey - 1] == 1) temp++;
+                if (tab[sizex-1, y] == 1) temp++;
+                if (tab[sizex-1, y+1] == 1) temp++;
+            }
+            else if (x == 0 && y == sizey - 1)
+            {
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x + 1,y - 1] == 1) temp++;
+                if (tab[0,0] == 1) temp++;
+                if (tab[1,0] == 1) temp++;
+                if (tab[sizex-1, y-1] == 1) temp++;
+                if (tab[sizex - 1, y ] == 1) temp++;
+                if (tab[sizex-1, 0] == 1) temp++;
+            }
+            else if (x == sizex - 1 && y == 0)
+            {
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x - 1,y + 1] == 1) temp++;
+                if (tab[x,sizey - 1] == 1) temp++;
+                if (tab[x - 1,sizey - 1] == 1) temp++;
+                if (tab[0, 0] == 1) temp++;
+                if (tab[0, 1] == 1) temp++;
+                if (tab[0, sizey-1] == 1) temp++;
+            }
+            else if (x == sizex - 1 && y == sizey - 1)
+            {
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x - 1,y - 1] == 1) temp++;
+                if (tab[sizex - 1,0] == 1) temp++;
+                if (tab[sizex - 2,0] == 1) temp++;
+                if (tab[0, sizey-2] == 1) temp++;
+                if (tab[0, sizey-1] == 1) temp++;
+                if (tab[0, 0] == 1) temp++;
+            }
+            else if (x == 0 && y != 0 && y != sizey - 1)
+            {
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x + 1,y - 1] == 1) temp++;
+                if (tab[x + 1,y + 1] == 1) temp++;
+                if (tab[sizex-1, y] == 1) temp++;
+                if (tab[sizex - 1, y - 1] == 1) temp++;
+                if (tab[sizex - 1, y + 1] == 1) temp++;
+            }
+            else if (x == sizex - 1 && y != 0 && y != sizey - 1)
+            {
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x - 1,y - 1] == 1) temp++;
+                if (tab[x - 1,y + 1] == 1) temp++;
+                if (tab[0, y] == 1) temp++;
+                if (tab[0, y - 1] == 1) temp++;
+                if (tab[0, y + 1] == 1) temp++;
+            }
+            else if (x != 0 && x != sizex - 1 && y == 0)
+            {
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x - 1,y + 1] == 1) temp++;
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x + 1,y + 1] == 1) temp++;
+                if (tab[x - 1,sizey - 1] == 1) temp++;
+                if (tab[x,sizey - 1] == 1) temp++;
+                if (tab[x + 1,sizey - 1] == 1) temp++;
+            }
+            else if (x != 0 && x != sizex - 1 && y == sizey - 1)
+            {
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x - 1,y - 1] == 1) temp++;
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x + 1,y - 1] == 1) temp++;
+                if (tab[x - 1,0] == 1) temp++;
+                if (tab[x,0] == 1) temp++;
+                if (tab[x + 1,0] == 1) temp++;
+            }
+            else
+            {
+                if (tab[x - 1,y - 1] == 1) temp++;
+                if (tab[x,y - 1] == 1) temp++;
+                if (tab[x + 1,y - 1] == 1) temp++;
+                if (tab[x - 1,y] == 1) temp++;
+                if (tab[x + 1,y] == 1) temp++;
+                if (tab[x - 1,y + 1] == 1) temp++;
+                if (tab[x,y + 1] == 1) temp++;
+                if (tab[x + 1,y + 1] == 1) temp++;
+            }
+
+            if (cell == 0 && temp == 3)
+                tab2[x,y] = 1;
+            if (cell == 1 && temp > 1 && temp < 4)
+                tab2[x,y] = 1;
+            if (cell == 1 && temp < 2 && temp > 3)
+                tab2[x,y] = 0;
+
+        }
+        int isAlive(int x, int y)
+        {
+            if (tab[x,y] == 1)
+                return 1;
+            else
+                return 0;
+        }
+        void draw()
+        {
+            for(int i=0;i<=px*sizex;i+=px)
+            {
+                for (int j = 0; j <= px * sizey; j += px)
+                    if (tab[i / px, j / px] == 1)
+                    {
+                        gr.FillRectangle(brush, j, i, px, px);
+                    }
+            }
+            pictureBox1.Image = bmp;
+        }
+        void move(int moves, int l)
+        {
+            if (l < moves)
+            {
+                gr.Clear(pictureBox1.BackColor);
+                next();
+                draw();
+            }
+            else
+            {
+                T.Stop();
+                licz = -1;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            T.Start();
+        }
+        double rnd()
+        {
+            
+            double x = rand.NextDouble();
+            return x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            px = Int32.Parse(textBox5.Text);
+            pictureBox1.Width = px * Int32.Parse(textBox1.Text);
+            pictureBox1.Height = px * Int32.Parse(textBox6.Text);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr = Graphics.FromImage(bmp);
+            sizey = Int32.Parse(textBox1.Text);
+            sizex = Int32.Parse(textBox6.Text);
+            tab = new int[sizex+1, sizey+1];
+            tab2 = new int[sizex+1, sizey+1];
+            
+            for (int i=0;i<=sizex;i++)
+            {
+                for (int j = 0; j <= sizey; j++)
+                {
+                    tab[i, j] = 0;
+                    tab2[i, j] = 0;
+                }
+            }
+            if(comboBox1.Text=="niezmienny")
+            {
+                tab[1, 2] = 1;
+                tab[1, 3] = 1;
+                tab[2, 1] = 1;
+                tab[2, 4] = 1;
+                tab[3, 2] = 1;
+                tab[3, 3] = 1;
+                draw();
+            }
+            else if (comboBox1.Text == "glider")
+            {
+                tab[1, 2] = 1;
+                tab[1, 3] = 1;
+                tab[2, 1] = 1;
+                tab[2, 2] = 1;
+                tab[3, 3] = 1;
+                draw();
+            }
+            else if (comboBox1.Text == "oscylator")
+            {
+                tab[2, 2] = 1;
+                tab[3, 2] = 1;
+                tab[4, 2] = 1;
+                draw();
+            }
+            else if (comboBox1.Text == "losowy")
+            {
+                for(int i = 0;i<=sizex;i++)
+                {
+                    for (int j = 0; j <= sizey; j++)
+                    {
+                        double x = rnd();
+                        if (x < 0.4)
+                        {
+                            tab[i, j] = 1;
+                        }
+
+                    }
+                }
+                draw();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            T.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tab[Int32.Parse(textBox2.Text), Int32.Parse(textBox3.Text)] = 1;
+            draw();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= sizex; i++)
+            {
+                for (int j = 0; j <= sizey; j++)
+                {
+                    tab[i, j] = 0;
+                    tab2[i, j] = 0;
+                }
+            }
+            pictureBox1.Image = null;
+        }
+    }
+}
