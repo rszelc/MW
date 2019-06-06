fc329ca36332b9640ab3c2f66471f71063884e64
rszelc <37050723+rszelc@users.noreply.github.com>
2019-06-06 17:32:52 +0200

    Add files via upload

---
+++ Automaty 1D/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rozmiar planszy:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Reguła:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "30",
+            "60",
+            "90",
+            "120",
+            "225"});
+            this.comboBox1.Location = new System.Drawing.Point(335, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "30";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 343);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ruchy:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(555, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Automaty 1D/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Black, 1);
+        SolidBrush brush = new SolidBrush(Color.Black);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void move90(int x, int[,] tab, int[,] tab2, int size)
+        {
+            for (int k = 0; k < x; k++)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if ((tab[i - 1,size - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 0) || (tab[i - 1,size - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 0) || (tab[i - 1,size - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 1) || (tab[i - 1,size - 1] == 0 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                        else if (j == (size - 1))
+                        {
+                            if ((tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,0] == 0) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,0] == 0) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,0] == 1) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 0 && tab[i - 1,0] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if ((tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 0) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 0) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 1) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i,j] = tab2[i,j];
+                    }
+                }
+            }
+        }
+        void move120(int x, int[,] tab, int[,] tab2, int size)
+        {
+            for (int k = 0; k < x; k++)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if ((tab[i - 1,size - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 0) || (tab[i - 1,size - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 1) || (tab[i - 1,size - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 0) || (tab[i - 1,size - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                        else if (j == (size - 1))
+                        {
+                            if ((tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,0] == 0) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,0] == 1) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,0] == 0) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,0] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if ((tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 0) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 1) || (tab[i - 1,j - 1] == 1 && tab[i - 1,j] == 0 && tab[i - 1,j + 1] == 0) || (tab[i - 1,j - 1] == 0 && tab[i - 1,j] == 1 && tab[i - 1,j + 1] == 1))
+                            {
+                                tab2[i,j] = 1;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i,j] = tab2[i,j];
+                    }
+                }
+            }
+        }
+        void move30(int x, int[,] tab, int[,] tab2, int size)
+        {
+            for (int k = 0; k < x; k++)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if ((tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                        else if (j == (size - 1))
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 1))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = tab2[i, j];
+                    }
+                }
+            }
+        }
+        void move60(int x, int[,] tab, int[,] tab2, int size)
+        {
+            for (int k = 0; k < x; k++)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if ((tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1) || (tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                        else if (j == (size - 1))
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 1) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = tab2[i, j];
+                    }
+                }
+            }
+        }
+        void move225(int x, int[,] tab, int[,] tab2, int size)
+        {
+            for (int k = 0; k < x; k++)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if ((tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0) || (tab[i - 1, size - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1) || (tab[i - 1, size - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                            else
+                                tab2[i, j] = 0;
+                        }
+                        else if (j == (size - 1))
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 1) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, 0] == 0) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, 0] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                            else
+                                tab2[i, j] = 0;
+                        }
+                        else
+                        {
+                            if ((tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 1) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 1 && tab[i - 1, j + 1] == 0) || (tab[i - 1, j - 1] == 1 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 1) || (tab[i - 1, j - 1] == 0 && tab[i - 1, j] == 0 && tab[i - 1, j + 1] == 0))
+                            {
+                                tab2[i, j] = 1;
+                            }
+                            else
+                                tab2[i, j] = 0;
+
+                        }
+                        
+                    }
+
+                    for (int a = 0; a < size; a++)
+                    {
+                        for (int b = 0; b < size; b++)
+                        {
+                            tab[a, b] = tab2[a, b];
+                        }
+                    }
+                }
+                
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int px = Int32.Parse(textBox2.Text);
+            pictureBox1.Width = px*Int32.Parse(textBox1.Text);
+            pictureBox1.Height = px*Int32.Parse(textBox1.Text);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            if(Int32.Parse(comboBox1.Text)==90)
+            {
+                int size = Int32.Parse(textBox1.Text);
+                int moves = Int32.Parse(textBox3.Text);
+                int[,] tab = new int[size, size];
+                int[,] tab2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i,j] = 0;
+                        tab2[i,j] = 0;
+                    }
+                }
+                tab[1, 20] = 1;
+                gr.FillRectangle(brush, px*20, px*1, px*1, px*1);
+                move90(moves, tab, tab2, size);
+                
+                for(int i=0;i<px*size;i+=4)
+                {
+                    for(int j=0;j<px*size;j+=4)
+                    {
+                        if (tab[i/px, j/px] == 1)
+                            gr.FillRectangle(brush, j, i, px, px);
+                    }
+
+                }
+            }
+            if (Int32.Parse(comboBox1.Text) == 30)
+            {
+                int size = Int32.Parse(textBox1.Text);
+                int moves = Int32.Parse(textBox3.Text);
+                int[,] tab = new int[size, size];
+                int[,] tab2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = 0;
+                        tab2[i, j] = 0;
+                    }
+                }
+                tab[1, 20] = 1;
+                gr.FillRectangle(brush, px * 20, px * 1, px * 1, px * 1);
+                move30(moves, tab, tab2, size);
+
+                for (int i = 0; i < px * size; i += 4)
+                {
+                    for (int j = 0; j < px * size; j += 4)
+                    {
+                        if (tab[i / px, j / px] == 1)
+                            gr.FillRectangle(brush, j, i, px, px);
+                    }
+
+                }
+            }
+            if (Int32.Parse(comboBox1.Text) == 120)
+            {
+                int size = Int32.Parse(textBox1.Text);
+                int moves = Int32.Parse(textBox3.Text);
+                int[,] tab = new int[size, size];
+                int[,] tab2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = 0;
+                        tab2[i, j] = 0;
+                    }
+                }
+                tab[1, 20] = 1;
+                gr.FillRectangle(brush, px * 20, px * 1, px * 1, px * 1);
+                move120(moves, tab, tab2, size);
+
+                for (int i = 0; i < px * size; i += 4)
+                {
+                    for (int j = 0; j < px * size; j += 4)
+                    {
+                        if (tab[i / px, j / px] == 1)
+                            gr.FillRectangle(brush, j, i, px, px);
+                    }
+
+                }
+            }
+            if (Int32.Parse(comboBox1.Text) == 60)
+            {
+                int size = Int32.Parse(textBox1.Text);
+                int moves = Int32.Parse(textBox3.Text);
+                int[,] tab = new int[size, size];
+                int[,] tab2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = 0;
+                        tab2[i, j] = 0;
+                    }
+                }
+                tab[1, 20] = 1;
+                gr.FillRectangle(brush, px * 20, px * 1, px * 1, px * 1);
+                move60(moves, tab, tab2, size);
+
+                for (int i = 0; i < px * size; i += 4)
+                {
+                    for (int j = 0; j < px * size; j += 4)
+                    {
+                        if (tab[i / px, j / px] == 1)
+                            gr.FillRectangle(brush, j, i, px, px);
+                    }
+
+                }
+            }
+            if (Int32.Parse(comboBox1.Text) == 225)
+            {
+                int size = Int32.Parse(textBox1.Text);
+                int moves = Int32.Parse(textBox3.Text);
+                int[,] tab = new int[size, size];
+                int[,] tab2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        tab[i, j] = 0;
+                        tab2[i, j] = 0;
+                    }
+                }
+                tab[0, 20] = 1;
+                tab2[0, 20] = 1;
+                //gr.FillRectangle(brush, px * 20, px * 3, px * 1, px * 1);
+                move225(moves, tab, tab2, size);
+
+                for (int i = 0; i < px * size; i += 4)
+                {
+                    for (int j = 0; j < px * size; j += 4)
+                    {
+                        if (tab[i / px, j / px] == 1)
+                            gr.FillRectangle(brush, j, i, px, px);
+                    }
+
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
